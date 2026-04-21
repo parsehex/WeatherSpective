240225b75ae88827b4604c7cb252ed8e578c7cce
parsehex <1779937+parsehex@users.noreply.github.com>
2026-04-20 20:39:39 -0400

    attempted controls fix

--- src/lib/runtimeConfig.ts
+++ src/lib/runtimeConfig.ts
@@ -73,4 +73,4 @@ export const hasInvalidPrecipitationUnitLockConfig = Boolean(lockedPrecipUnitRaw
 
 export const parseboxControlsUrl = parseboxControlsUrlRaw && parseboxControlsUrlRaw.length > 0
   ? parseboxControlsUrlRaw
-  : 'http://127.0.0.1:4174/controls'
+  : 'http://127.0.0.1:4175/'
