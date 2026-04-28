03305c5a2da02b39866b096d1a0e6dc9697f7547
parsehex <1779937+parsehex@users.noreply.github.com>
2026-04-28 09:21:03 -0400

    try to fix gh pages build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-	const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+	const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
 	const base = isGitHubActions && repoName ? `/${repoName}/` : '/';
 
 	return {
